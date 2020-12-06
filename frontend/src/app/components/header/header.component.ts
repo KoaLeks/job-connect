a7955446d6383946548da45b0c41c11ab51a9c67
Stefanie Feldmann <e01609368@student.tuwien.ac.atgit config --global user.email e1609368@student.tuwien.ac.at>
2020-12-06 17:13:33 +0100

    added profile picture to header (when logged in)

--- frontend/src/app/components/header/header.component.ts
+++ frontend/src/app/components/header/header.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {AuthService} from '../../services/auth.service';
+import {EmployerService} from '../../services/employer.service';
 
 @Component({
   selector: 'app-header',
@@ -9,10 +10,46 @@ import {AuthService} from '../../services/auth.service';
 export class HeaderComponent implements OnInit {
 
   isEmployee: boolean;
+  picture: any;
+  profile: any;
+  hasPicture =  false;
+  error = false;
+  errorMessage;
 
-  constructor(public authService: AuthService) { }
+  constructor(public authService: AuthService, public employerService: EmployerService) { }
 
   ngOnInit() {
+    if (this.authService.isLoggedIn()) {
+      this.loadEmployerPicture();
+    }
+  }
+
+  private loadEmployerPicture() {
+    this.employerService.getEmployerByEmail(this.authService.getTokenIdentifier()).subscribe(
+      (profile: any) => {
+        this.profile = profile;
+        // converts bytesArray to Base64
+        this.arrayBufferToBase64(profile.profileDto.picture);
+        if (profile.profileDto.picture != null) {
+          this.picture = 'data:image/png;base64,' + this.picture;
+          this.hasPicture = true;
+        }
+        console.log(profile);
+      },
+      error => {
+        this.error = true;
+        this.errorMessage = error.error;
+      }
+    );
+  }
+  private arrayBufferToBase64(buffer) {
+    let binary = '';
+    const bytes = new Uint8Array(buffer);
+    const len = bytes.byteLength;
+    for (let i = 0; i < len; i++) {
+      binary += String.fromCharCode(bytes[i]);
+    }
+    this.picture = window.btoa(binary);
   }
 
 }
