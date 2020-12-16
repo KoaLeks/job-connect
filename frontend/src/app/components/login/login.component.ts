0c310563ad6e1750c3065629fc94afa4bb0f3d17
Dominik Gurschler <e1631847@student.tuwien.ac.at>
2020-12-16 15:46:34 +0100

    clear login form afer succesful login and show email validation info

--- frontend/src/app/components/login/login.component.ts
+++ frontend/src/app/components/login/login.component.ts
@@ -7,19 +7,19 @@ import {UpdateHeaderService} from '../../services/update-header.service';
 
 
 @Component({
-    selector: 'app-login',
-    templateUrl: './login.component.html',
-    styleUrls: ['./login.component.scss']
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
 
-    @Input() isEmployee: boolean;
-    loginForm: FormGroup;
-    // After first submission attempt, form validation will start
-    submitted: boolean = false;
-    // Error flag
-    error: boolean = false;
-    errorMessage: string = '';
+  @Input() isEmployee: boolean;
+  loginForm: FormGroup;
+  // After first submission attempt, form validation will start
+  submitted: boolean = false;
+  // Error flag
+  error: boolean = false;
+  errorMessage: string = '';
 
     constructor(private formBuilder: FormBuilder, private authService: AuthService, private router: Router,
                 private updateHeaderService: UpdateHeaderService) {
@@ -29,71 +29,72 @@ export class LoginComponent implements OnInit {
         });
     }
 
-    /**
-     * Clears Form
-     */
-    clearForm(): void {
-        this.submitted = false;
-        this.loginForm.reset();
-        this.vanishError();
-    }
+  /**
+   * Clears Form
+   */
+  clearForm(): void {
+    this.submitted = false;
+    this.loginForm.reset();
+    this.vanishError();
+  }
 
-    /**
-     * Form validation will start after the method is called, additionally an AuthRequest will be sent
-     */
-    loginUser() {
-        this.submitted = true;
-        // removes previous errors
+  /**
+   * Form validation will start after the method is called, additionally an AuthRequest will be sent
+   */
+  loginUser() {
+    this.submitted = true;
+    // removes previous errors
+    this.vanishError();
+    if (this.loginForm.valid) {
+      const authRequest: AuthRequest = new AuthRequest(this.loginForm.controls.email.value, this.loginForm.controls.password.value);
+      this.authenticateUser(authRequest);
+    } else {
+      console.log('Invalid input');
+    }
+  }
+
+  /**
+   * Send authentication data to the authService. If the authentication was successfully, the user will be forwarded to the message page
+   * @param authRequest authentication data from the user login form
+   */
+  authenticateUser(authRequest: AuthRequest) {
+    console.log('Try to authenticate user: ' + authRequest.email);
+    this.authService.loginUser(authRequest).subscribe(
+      () => {
+        console.log('Successfully logged in user: ' + authRequest.email);
+        // @ts-ignore
+        $('#loginModal').modal('hide');
+        this.updateHeaderService.updateProfile.next(true);
+        this.updateHeaderService.emitNewLoggedInUser(true);
         this.vanishError();
-        if (this.loginForm.valid) {
-            const authRequest: AuthRequest = new AuthRequest(this.loginForm.controls.email.value, this.loginForm.controls.password.value);
-            this.authenticateUser(authRequest);
+        this.router.navigate(['/']);
+        this.clearForm();
+      },
+      error => {
+        console.log('Could not log in due to:');
+        console.log(error);
+        this.error = true;
+        if (error.status === 401) {
+          this.errorMessage = 'E-Mail und Passwort stimmen nicht überein';
         } else {
-            console.log('Invalid input');
+          if (typeof error.error === 'object') {
+            this.errorMessage = error.error.error;
+          } else {
+            this.errorMessage = error.error;
+          }
         }
-    }
+      }
+    );
+  }
 
-    /**
-     * Send authentication data to the authService. If the authentication was successfully, the user will be forwarded to the message page
-     * @param authRequest authentication data from the user login form
-     */
-    authenticateUser(authRequest: AuthRequest) {
-        console.log('Try to authenticate user: ' + authRequest.email);
-        this.authService.loginUser(authRequest).subscribe(
-            () => {
-              console.log('Successfully logged in user: ' + authRequest.email);
-              // @ts-ignore
-                $('#loginModal').modal('hide');
-              this.updateHeaderService.updateProfile.next(true);
-              this.updateHeaderService.emitNewLoggedInUser(true);
-              this.vanishError();
-              this.router.navigate(['/']);
-            },
-            error => {
-                console.log('Could not log in due to:');
-                console.log(error);
-                this.error = true;
-                if (error.status === 401) {
-                    this.errorMessage = 'E-Mail und Passwort stimmen nicht überein';
-                } else {
-                    if (typeof error.error === 'object') {
-                        this.errorMessage = error.error.error;
-                    } else {
-                        this.errorMessage = error.error;
-                    }
-                }
-            }
-        );
-    }
+  /**
+   * Error flag will be deactivated, which clears the error message
+   */
+  vanishError() {
+    this.error = false;
+  }
 
-    /**
-     * Error flag will be deactivated, which clears the error message
-     */
-    vanishError() {
-        this.error = false;
-    }
-
-    ngOnInit() {
-    }
+  ngOnInit() {
+  }
 
 }
