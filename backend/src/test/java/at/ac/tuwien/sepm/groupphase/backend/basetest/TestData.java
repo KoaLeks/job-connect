bd1a439a6ba5d1171aec7f6ed75b5b2c4efff416
Dominik Gurschler <e1631847@student.tuwien.ac.at>
2021-01-26 11:07:43 +0100

    Extend profile endpoint tests and add image resource folders to gitignore

--- backend/src/test/java/at/ac/tuwien/sepm/groupphase/backend/basetest/TestData.java
+++ backend/src/test/java/at/ac/tuwien/sepm/groupphase/backend/basetest/TestData.java
@@ -237,5 +237,14 @@ public interface TestData {
             add("ROLE_EMPLOYEE");
         }
     };
-
+    List<String> EMPLOYER_ROLES = new ArrayList<>() {
+        {
+            add("ROLE_EMPLOYER");
+        }
+    };
+    List<String> EMPLOYEE_ROLES = new ArrayList<>() {
+        {
+            add("ROLE_EMPLOYEE");
+        }
+    };
 }
